c908967e7a6c0c814517f521a84ba934caaa04f1
JaoVicy <filipimjoa@gmail.com>
2024-12-16 15:33:05 -0300

    Initial Commit

--- Estudos.playground/Contents.swift
+++ Estudos.playground/Contents.swift
@@ -1,1 +1,20 @@
 
+// Created by João V. Benetti F.
+
+import UIKit
+
+/// Testes da linguagem:
+
+var greeting = "Hello, playground"
+print(greeting)
+                
+var name = "João"
+    
+print(name)
+
+if (-1 > 0){
+    print(name)
+} else {
+    print(greeting)
+}
+
